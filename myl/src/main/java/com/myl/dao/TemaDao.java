7f7c55e34322faf60cbd4331d277269157198f22
csantiagoalva <csantiagoalva@gmail.com>
2013-06-16 23:33:33 -0500

    Proyecto Myl

---
+++ myl/src/main/java/com/myl/dao/TemaDao.java
@@ -0,0 +1,41 @@
+package com.myl.dao;
+
+import java.util.List;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import com.myl.modelo.Tema;
+
+@Singleton
+@Named("temaDao")
+public class TemaDao extends HibernateDaoSupport {
+	public List<Tema> findAll() {		
+		return getHibernateTemplate().loadAll(Tema.class);
+	}
+
+	public Tema findById(Integer id) {
+		return getHibernateTemplate().get(Tema.class, id);
+	}
+
+	public Tema save(Tema entity) {
+		if (entity.getIdTema() != null) {
+			entity = getHibernateTemplate().merge(entity);
+		}
+		getSession().saveOrUpdate(entity);
+		return entity;
+	}
+
+	public void delete(Tema entity) {
+		getHibernateTemplate().refresh(entity);
+		getSession().delete(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Tema> findByExample(Tema tema) { // TODO:
+		return getHibernateTemplate().findByExample(tema);
+	}
+}
---
+++ myl/src/main/java/com/myl/modelo/Tema.java
@@ -0,0 +1,60 @@
+package com.myl.modelo;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlIDREF;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+
+@XmlRootElement
+@Entity
+@Table(name = "Tema")
+public class Tema {
+	private Integer idTema;
+	private String nombre;
+	private String descripcion;	
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@Column(name = "id_tema")
+	public Integer getIdTema() {
+		return idTema;
+	}
+	public void setIdTema(Integer idTema) {
+		this.idTema = idTema;
+	}
+	
+	@Column(name = "nb_tema")
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	@Column(name = "ds_tema")
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+		
+	
+	
+	
+		
+}
